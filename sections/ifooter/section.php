63bd56a895e43b7427a3dd30c828c7189aa9ea2d
Raymond Lopez Aleman <hello@raylo.net>
2014-05-27 16:34:22 -0500

    ifooter

--- sections/ifooter/section.php
+++ sections/ifooter/section.php
@@ -14,37 +14,11 @@ class iFooter extends PageLinesSection {
 	
 	function section_opts(){
 
-		$menu_options = array(); 
-		
-		for( $i = 1; $i <= 3; $i++ ){
-			
-			$menu_options[] = array(
-									'key'			=> 'pf_nav_title_'.$i,
-									'type'			=> 'text',
-									'label'		 	=> sprintf( __( 'Nav %s Title', 'pagelines' ), $i ),
-								);
-			
-			$menu_options[] = array(
-									'key'			=> 'pf_nav_menu_'.$i,
-									'type'			=> 'select_menu',
-									'label'		 	=> sprintf( __( 'Nav %s Menu', 'pagelines' ), $i ),
-								);
-			
-			$menu_options[] = array(
-									'type'			=> 'divider',
-								);
-		}
-
 		$options = array(
 		 	array(
 				'type' 			=> 'multi',
 				'title' 		=> __( 'Logo', 'pagelines' ),
 				'opts'			=> array(
-					array(
-						'key'		=> 'logo',
-						'type' 		=> 'image_upload',
-						'label' 	=> __( 'Logo', 'pagelines' ),
-					),
 					
 					array(
 						'key'		=> 'text',
@@ -65,7 +39,14 @@ class iFooter extends PageLinesSection {
 				'type' 			=> 'multi',
 				'col'			=> 2,
 				'title' 		=> __( 'Navigation Columns', 'pagelines' ),
-				'opts'			=> $menu_options
+				'opts'			=> array(
+
+					array(
+						'key'			=> 'if_nav_menu',
+						'type'			=> 'select_menu',
+						'label'		 	=> __( 'Nav Menu', 'pagelines' ),
+					),
+				)
 			),
 			
 			
@@ -77,38 +58,11 @@ class iFooter extends PageLinesSection {
 	
    function section_template() { 
 	
-		$logo = ( $this->opt('logo') ) ? $this->opt('logo') : PL_THEME_URL.'/logo.png';
 		$text = ( $this->opt('text') ) ? $this->opt('text') : 'Designed by PageLines in California.';
 		
-		$copy = ( $this->opt('copy') ) ? $this->opt('copy') : '&copy; iBlogPro 2014';
-		
-		
-		$cols = array(); 
-		for( $i = 1; $i <= 3; $i++ ){
-			
-			$menu =  ( $this->opt('pf_nav_menu_'.$i) ) ? $this->opt('pf_nav_menu_'.$i) : false;
-			
-			
-			
-			$cols[ $i ] = array(
-				'menu'		=> false, 
-				'title'		=> false
-			);
-			
-			if( $menu && is_array( wp_get_nav_menu_items( $menu ) ) ){
-				$args = array(
-					'menu'            	=> '',
-					'echo'            	=> false,
-					'items_wrap'      	=> '%3$s',
-					'container'			=> ''
-				);
-				$cols[ $i ]['menu'] = wp_nav_menu( $args );
-			}
-				
-			
-			$cols[ $i ]['title'] = ( $this->opt('pf_nav_title_'.$i) ) ? $this->opt('pf_nav_title_'.$i) : false;
-		
-		}
+		$copy = ( $this->opt('copy') ) ? $this->opt('copy') : 'Copyright &copy; 2014 iBlogPro. All rights reserved';
+
+		$menu = ( $this->opt('if_nav_menu') ) ? $this->opt('if_nav_menu') : false; 
 		
 	
 	?>
@@ -116,9 +70,6 @@ class iFooter extends PageLinesSection {
 	<div class="ifooter-container row">
 		<div class="span4">
 			
-			<div class="logo">
-				<a href="<?php echo home_url();?>"><img src="<?php echo $logo; ?>" /></a>
-			</div>
 			<div class="tagline-text">
 				<p><?php echo $text; ?></p>
 			</div>
@@ -126,33 +77,25 @@ class iFooter extends PageLinesSection {
 				<p><?php echo $copy; ?></p>
 			</div>
 		</div>
-		<div class="span2 offset2">
-			<?php 
-			
-				$title = ( $cols[1]['title'] ) ? $cols[1]['title'] : __('Pages','pagelines'); 
-				$menu = ( $cols[1]['menu'] ) ? $cols[1]['menu'] : pl_list_pages(); 
-				
-				echo pl_media_list( $title, $menu ); 
-			?>
-			
-		</div>
-		<div class="span2">
-			<?php 
-			
-				$title = ( $cols[2]['title'] ) ? $cols[2]['title'] : __('Categories','pagelines'); 
-				$menu = ( $cols[2]['menu'] ) ? $cols[2]['menu'] : pl_popular_taxonomy(); 
-				
-				echo pl_media_list( $title, $menu ); 
-			?>
-		</div>
-		<div class="span2">
-			<?php 
-			
-				$title = ( $cols[3]['title'] ) ? $cols[3]['title'] : __('Tags','pagelines'); 
-				$menu = ( $cols[3]['menu'] ) ? $cols[3]['menu'] : pl_popular_taxonomy( 6, 'post_tag'); 
-				
-				echo pl_media_list( $title, $menu ); 
-			?>
+		<div class="span8">
+
+	       		
+	       		<?php 	
+					if ( is_array( wp_get_nav_menu_items( $menu ) ) || has_nav_menu( 'if_nav_menu' ) ) {
+					wp_nav_menu(
+						array(
+							'menu_class'		=> 'ifooter_nav',
+							'menu'				=> $menu,
+							'container'			=> null,
+							'depth'				=> 1,
+							'fallback_cb'		=> '',
+							'theme_location'	=> 'main_nav',
+						)
+					);
+					} else 
+						pl_nav_fallback('ifooter_nav');
+					?>
+
 		</div>
 	</div>
 	
