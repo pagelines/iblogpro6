a09b2660bfae17423a9e5d7bd5e1e08c01994a2a
Raymond Lopez Aleman <hello@raylo.net>
2014-07-08 16:23:44 -0500

    light/dark navigation

--- sections/islider/islider.js
+++ sections/islider/islider.js
@@ -10,7 +10,18 @@
 	        controlNav: false,  
 	        //smoothHeight: true,
 	        touch: true,
+	        
+	        after: function(slider) {
+      			
+      			console.log(slider)
 
+      			var slide = slider.find('.flex-active-slide .content')
+
+      			if( slide.hasClass('element-dark') )
+      				slider.find('.flex-direction-nav').addClass('dark-nav')
+      			else 
+      				slider.find('.flex-direction-nav').removeClass('dark-nav')
+    		}
 	      });
 
 	})
