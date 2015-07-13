e5d9e971a0bb5775b0176d86509e4cf07ed1e1a4
Simon <pross@pross.org.uk>
2015-07-13 19:46:48 +0100

    respect SEO h2 to h1

--- sections/iblog/section.php
+++ sections/iblog/section.php
@@ -11,7 +11,7 @@
 
 class iBlog extends PageLinesSection {
 
-	
+
 	function before_section_template( $location = '' ) {
 
 		global $wp_query;
@@ -22,40 +22,40 @@ class iBlog extends PageLinesSection {
 	}
 
 	function section_opts(){
-		
+
 		$options = array();
 
-		
+
 
 		return $options;
-		
+
 	}
 
 	/**
 	* Section template.
 	*/
    function section_template() {
-	
+
 		if( have_posts() )
 			while ( have_posts() ) : the_post();  $this->get_article(); endwhile;
 		else
 			$this->posts_404();
-	
+
 	}
-	
+
 	function get_article(){
 		$format = get_post_format();
 
 		$linkbox = ($format == 'quote' || $format == 'link') ? true : false;
-		
+
 		$gallery_format = get_post_meta( get_the_ID(), '_pagelines_gallery_slider', true);
 
 		$class[ ] = ( ! empty( $gallery_format ) ) ? 'use-flex-gallery' : '';
-		
+
 		$classes = apply_filters( 'pagelines_get_article_post_classes', join( " ", $class) );
 
 		$post_id = get_the_ID();
-		
+
 		?>
 		<div class="row fix ipost">
 			<div class="span1 ipost-date">
@@ -63,27 +63,27 @@ class iBlog extends PageLinesSection {
 					<span class="month"><?php echo do_shortcode( '[post_date format="M"]' ); ?></span>
 			</div>
 			<div class="span10 offset1">
-				
+
 				<article id="post-<?php the_ID(); ?>" <?php post_class( $classes ); ?>>
-					
+
 					<?php
-						$media = pagelines_media( array( 'thumb-size' => 'aspect-thumb' ) ); 
-						
+						$media = pagelines_media( array( 'thumb-size' => 'aspect-thumb' ) );
+
 						if( ! empty( $media ) )
 							printf( '<div class="metamedia">%s</div>', $media );
-					
+
 					?>
-				
+
 					<?php if( ! $linkbox || is_single() ): ?>
 					<div class="ipost-text">
 						<?php if( ! $linkbox  ): ?>
-							<h2 class="title"><a href="<?php the_permalink(); ?>"><?php echo get_the_title(); ?></a></h2>
+							<h1 class="title"><a href="<?php the_permalink(); ?>"><?php echo get_the_title(); ?></a></h1>
 						<?php endif; ?>
 							<div class="content">
 								<div class="author-name">Posted by <?php echo do_shortcode( '[post_author_posts_link]' ); ?> / <?php echo do_shortcode( '[post_categories]' ); ?></div>
 
-								<?php 
-									if( ! is_single() ) 
+								<?php
+									if( ! is_single() )
 										echo get_the_excerpt();
 									else{
 										the_content('...');
@@ -98,14 +98,14 @@ class iBlog extends PageLinesSection {
 								?>
 
 								<?php echo do_shortcode( '[post_tags]' ); ?>
-								
+
 							</div>
 					</div>
 					<?php endif; ?>
 				</article>
 			</div>
 		</div>
-		<?php 
+		<?php
 	}
 
 
@@ -120,5 +120,5 @@ class iBlog extends PageLinesSection {
 		printf( '<section class="billboard">%s <div class="center fix">%s</div></section>', apply_filters('pagelines_posts_404', $the_text), pagelines_search_form( false ));
 
 	}
-	
+
 }
